830ff5be9ec9511f2154ca9e98a9f3064b7fcd12
Srinuas <srinivasvanapalli0499@gmail.com>
2026-01-08 15:18:56 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,9 +2,10 @@ sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
+sudo yum install java-21-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status Jenkins
 sudo systemctl status Jenkins
+cat /var/lib/jenkins/secrets/initialAdminPassword
