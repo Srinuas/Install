0546b80d77bc1ba26346ba0d645b3614c2964af3
Srinuas <srinivasvanapalli0499@gmail.com>
2026-01-08 15:01:04 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,12 +3,8 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo yum install java-25-amazon-corretto -y
-sudo yum install jenkins git -y
+sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status Jenkins
-sudo yum install docker -y
-sudo systemctl start docker
 sudo systemctl status Jenkins
-sudo systemctl status docker
-cat /var/lib/jenkins/secrets/initialAdminPassword
